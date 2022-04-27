116fa8d0d5e845b18d569b48717910e222507124
Andres Valdez <andresevaldez@gmail.com>
2022-04-27 10:57:01 -0500

    Moved handleIncrement function to Counters to be able to use reset

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,36 +1,31 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.handleIncrement = this.handleIncrement.bind(this);
-  // }
-
-  state = {
-    value: this.props.counter.value,
-  };
+  // state = {
+  //   value: this.props.counter.value,
+  // };
 
   styles = {
     fontSize: 30,
   };
 
   formatCount() {
-    const { value: value } = this.state;
+    const { value: value } = this.props.counter;
     return value === 0 ? "Zero" : value;
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
-  handleIncrement = (product) => {
-    console.log(product);
-    this.setState({
-      value: this.state.value + 1,
-    });
-  };
+  // handleIncrement = (product) => {
+  //   console.log(product);
+  //   this.setState({
+  //     value: this.state.value + 1,
+  //   });
+  // };
 
   render() {
     return (
@@ -40,15 +35,14 @@ class Counter extends Component {
           {this.formatCount()}
         </span>
         <button
-          onClick={() => this.handleIncrement("Passing string as parameter")}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary"
         >
           Increment
         </button>
         <button
           onClick={() => this.props.onDelete(this.props.counter.id)}
-          type="button"
-          className="btn btn-danger btn-sm m-2"
+          className="btn btn-danger btn-sm m-1"
         >
           Delete
         </button>
--- src/components/Counters.jsx
+++ src/components/Counters.jsx
@@ -4,10 +4,10 @@ import Counter from "./Counter";
 class Counters extends Component {
   state = {
     counters: [
-      { id: 1, value: 1 },
-      { id: 2, value: 2 },
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
       { id: 3, value: 0 },
-      { id: 4, value: 4 },
+      { id: 4, value: 0 },
     ],
   };
 
@@ -18,14 +18,48 @@ class Counters extends Component {
     const counter = this.state.counters.filter((c) => c.id !== counterId);
     this.setState({ counters: counter });
   };
+
+  handleReset = () => {
+    console.log("Handle Reset was pressed");
+    const resetCounter = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    console.log(resetCounter);
+    this.setState({ resetCounter });
+  };
+
+  handleIncrement = (counter) => {
+    console.log("Increment button pressed");
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+
+    this.setState({ counters });
+
+    //This tested to see if it didnt change the state, only the new counters array
+    //counters[0] = { ...stateCounter };
+    //counters[0].value++;
+    //console.log(this.state.counters[0]);
+  };
+
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
+
         {this.state.counters.map((counter) => (
           //  Counter initialized and onDelete prop is being used in "Counter" Component for info
           <Counter
             key={counter.id}
             onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
             counter={counter}
 
             //Was replaced by just moving the whole counter object state
